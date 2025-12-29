1dadd56748c1ddcd10934ea5696b176c56f48d3c
mohitjangra997 <mohitjangra997@gmail.com>
2025-12-29 08:20:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/a666/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.a666
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.a666.ui.theme._666Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _666Theme {
+              Surface(){
+
+              }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    _666Theme {
+
+    }
+}
