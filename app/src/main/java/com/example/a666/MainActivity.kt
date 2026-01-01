7a7d0d0e30176f53f71e40c46e3033ad0d73ae72
mohitjangra997 <mohitjangra997@gmail.com>
2026-01-01 10:47:41 +0530

    "Added Note data model and NoteScreen UI"

--- app/src/main/java/com/example/a666/MainActivity.kt
+++ app/src/main/java/com/example/a666/MainActivity.kt
@@ -38,6 +38,7 @@ class MainActivity : ComponentActivity() {
 }
 
 
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
--- app/src/main/java/com/example/a666/screen/NoteScreen.kt
+++ app/src/main/java/com/example/a666/screen/NoteScreen.kt
@@ -1,14 +1,20 @@
 import android.R.attr.contentDescription
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Notifications
@@ -17,6 +23,7 @@ import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
@@ -29,6 +36,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,7 +46,9 @@ import com.example.a666.R.string.app_name
 import com.example.a666.components.NoteInputText
 import com.example.a666.data.NotesDataSource
 import com.example.a666.model.Note
+import java.time.format.DateTimeFormatter
 
+@RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NoteScreen(
@@ -51,41 +61,46 @@ fun NoteScreen(
     var description by remember {
         mutableStateOf("")
     }
+    val scrollState = rememberScrollState()
 
-//    val notes = remember {
-//        mutableStateListOf(
-//            Note(title = "First Note", description = "Hello Mohit!"),
-//            Note(title = "Second Note", description = "This is a test")
-//        )
-//    }
-
-
+    val notes = remember {
+        mutableStateListOf(
+            Note(
+                title = "First Note",
+                description = "Hello JetPack compose , This is me Mohit Jangra , And i am here to learn you ,so that i can make a clear user interface for my apps ,Hello JetPack compose , This is me Mohit Jangra , And i am here to learn you ,so that i can make a clear user interface for my apps"
+            ),
+            Note(
+                title = "First Note",
+                description = "Hello JetPack compose , This is me Mohit Jangra , And i am here to learn you ,so that i can make a clear user interface for my apps "
+            ),
+            Note(
+                title = "First Note",
+                description = "Hello JetPack compose , This is me Mohit Jangra , And i am here to learn you ,so that i can make a clear user interface for my apps "
+            ),
+            Note(
+                title = "First Note",
+                description = "Hello JetPack compose , This is me Mohit Jangra , And i am here to learn you ,so that i can make a clear user interface for my apps "
+            ),
+        )
+    }
     Column(
-        modifier = Modifier.padding(6.dp)
+        modifier = Modifier
+            .padding(6.dp)
+            .verticalScroll(scrollState)
     ) {
-
-        LazyColumn(
-            modifier = Modifier.height(200.dp)
-        ) {
-            items(notes) { note ->
-                Text(
-                    text = note.title, modifier = Modifier.padding(8.dp)
-                )
-            }
-        }
         //top bar showing name of the app and a notification icon
         TopAppBar(
             title = {
-            Text(
-                text = stringResource(app_name), color = Color(0xFFEA4545)// text color - red
+                Text(
+                    text = stringResource(app_name), color = Color(0xFFEA4545)// text color - red
+                )
+            }, actions = {
+                Icon(
+                    imageVector = Icons.Rounded.Notifications, contentDescription = "Icon"
+                )
+            }, colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color(0xFFEFDC2D), //background color - yellow
             )
-        }, actions = {
-            Icon(
-                imageVector = Icons.Rounded.Notifications, contentDescription = "Icon"
-            )
-        }, colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = Color(0xFFEFDC2D), //background color - yellow
-        )
         )
         Divider(modifier = Modifier.height(2.dp))
         //creating text fields and a save button
@@ -95,13 +110,13 @@ fun NoteScreen(
         ) {
             NoteInputText(
                 modifier = Modifier.padding(
-                top = 9.dp,
-            ), TextValue = title, label = "Title", maxLines = 1, onTextChange = {
-                if (it.all { char ->
-                        char.isLetterOrDigit() || char.isWhitespace()
-                    }) title = it
+                    top = 9.dp,
+                ), TextValue = title, label = "Title", maxLines = 1, onTextChange = {
+                    if (it.all { char ->
+                            char.isLetterOrDigit() || char.isWhitespace()
+                        }) title = it
 
-            }, onImeAction = {}
+                }, onImeAction = {}
 
             )
             Divider(
@@ -111,13 +126,13 @@ fun NoteScreen(
             )
             NoteInputText(
                 modifier = Modifier.padding(
-                top = 9.dp,
-            ), TextValue = description, label = "Add a Note", maxLines = 1, onTextChange = {
-                if (it.all { char ->
-                        char.isWhitespace() || char.isLetterOrDigit()
-                    }) description = it
+                    top = 9.dp,
+                ), TextValue = description, label = "Add a Note", maxLines = 1, onTextChange = {
+                    if (it.all { char ->
+                            char.isWhitespace() || char.isLetterOrDigit()
+                        }) description = it
 
-            }, onImeAction = {})
+                }, onImeAction = {})
             Divider(
                 modifier = Modifier
                     .height(2.dp)
@@ -133,18 +148,58 @@ fun NoteScreen(
                     }
                 })
             Divider(modifier = Modifier.padding(10.dp))
+            LazyColumn(
+                modifier = Modifier.height(500.dp)
+            ) {
+                items(notes) { note ->
+                    NoteRow(
+                        note = note,
+                        onNoteClicked = {})
 
-            Text(text = "Mohit jangra")
 
-            Text(text = "Mohit jangra")
+                }
+            }
+
         }
     }
 }
 
-@Preview(showBackground = true)
+
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun NotesScreeenPreview() {
-    NoteScreen(notes = NotesDataSource().loadNotes(), onAddNote = {}, onRemoveNote = {})
+fun NoteRow(
+    modifier: Modifier = Modifier,
+    note: Note,
+    onNoteClicked: (Note) -> Unit
+) {
+    Surface(
+        modifier = Modifier
+            .padding(4.dp)
+            .heightIn(max = 150.dp)
+            .clip(RoundedCornerShape(30.dp))
+            .fillMaxWidth(0.8f),
+        color = Color(0xFF91D3EC),
+        shadowElevation = 6.dp
+    )
+    {
+        Column(
+            modifier
+                .clickable {}
+                .padding(
+                    horizontal = 14.dp,
+                    vertical = 6.dp
+                ),
+            horizontalAlignment = Alignment.Start)
+        {
+            Text(text = note.title)
+            Text(text = note.description)
+            Text(
+                text = note.entryDate.format(
+                    DateTimeFormatter.ofPattern("EEE, d MMM")
+                )
+            )
+        }
+    }
 }
 
 
